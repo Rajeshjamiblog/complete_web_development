d1de3255b8b5044a252264fc254f7a5926da3402
Rajeshjamiblog <rajeshjamiblog@gmail.com>
2024-12-28 11:38:01 +0530

    add explore1.js file

---
+++ advanced-js/explore1.js
@@ -0,0 +1,16 @@
+console.log("welcome")
+
+
+function sayHello() {
+    console.log("Hi Hello")
+}
+
+setTimeout(() => {
+    sayHello()
+}, 4000);
+
+for (let index = 0 ;index < 10 ;index++) {
+    console.log(index)
+}
+
+
